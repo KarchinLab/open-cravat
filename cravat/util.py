4d187742f8495487c2a80627b93d10cf69d16c43
kmoad <kmoad@insilico.us.com>
2018-09-10 17:33:02 -0400

    run Cravat from import

--- cravat/util.py
+++ cravat/util.py
@@ -173,4 +173,9 @@ def get_directory_size(start_path):
             total_size += os.path.getsize(fp)
     return total_size
 
-
+def get_argument_parser_defaults(parser):
+    return { 
+             action.dest : action.default
+             for action in parser._actions
+             if action.dest != 'help'
+            }
