f5af242115a9d7d6bc9cb2a57c786905986218ce
kmoad <kmoad@mail.insilico.us.com>
2019-05-02 13:09:04 -0400

    restrict install to python >= 3.6

--- setup.py
+++ setup.py
@@ -124,4 +124,5 @@ setup(
               },
     #install_requires=['pyyaml', 'requests', 'requests_toolbelt', 'pyliftover', 'websockets', 'markdown', 'aiohttp', 'aiohttp_session', 'cryptography'],
     install_requires=['pyyaml<5.1', 'requests', 'requests_toolbelt', 'pyliftover', 'websockets', 'markdown', 'aiohttp', 'chardet', 'aiosqlite3'],
+    python_requires='>=3.6',
 )
