8f93f55412bf2ef54ac95813335c80372efd89f2
Rick Kim <rkim@insilico.us.com>
2019-08-17 16:10:42 -0400

    added cravat-util migrate-result

--- cravat/cravat_util.py
+++ cravat/cravat_util.py
@@ -169,9 +169,142 @@ def migrate_result (args):
     except:
         print('Result DB is not open-cravat result DB or too old for migration')
         return
-    #if oc_ver in ['1.4.4', '1.4.5', '1.5.0', '1.5.1']:
-    print('@ oc_ver=', oc_ver)
-
+    if oc_ver not in ['1.4.4', '1.4.5', '1.5.0', '1.5.1']:
+        print('Result DB is too old for migration. OpenCRAVAT 1.4.4 and up are supported.')
+    # info
+    q = 'select colval from info where colkey="_converter_format"'
+    cursor.execute(q)
+    r = cursor.fetchone()
+    if r is None:
+        q = 'insert into info values ("_converter_format", "")'
+        cursor.execute(q)
+    q = 'select colval from info where colkey="_mapper"'
+    cursor.execute(q)
+    r = cursor.fetchone()
+    if r is None:
+        q = 'select colval from info where colkey="Gene mapper"'
+        cursor.execute(q)
+        r = cursor.fetchone()
+        hg38ver = r[0].split('(')[1].strip(')')
+        q = 'insert into info values ("_mapper", "hg38:{}")'.format(hg38ver)
+        cursor.execute(q)
+    q = 'select colval from info where colkey="_input_paths"'
+    cursor.execute(q)
+    r = cursor.fetchone()
+    if r is None:
+        q = 'select colval from info where colkey="Input file name"'
+        cursor.execute(q)
+        r = cursor.fetchone()
+        ips = r[0].split(';')
+        input_paths_str = '{'
+        for i in range(len(ips)):
+            input_paths_str += "'" + str(i) + "': '" + ips[i] + "', "
+        input_paths_str += '}'
+        q = 'insert into info values ("_input_paths", "{}")'.format(input_paths_str)
+        cursor.execute(q)
+    q = 'select colval from info where colkey="_annotator_desc"'
+    cursor.execute(q)
+    r = cursor.fetchone()
+    if r is None:
+        q = 'insert into info values ("_annotator_desc", "{}")'
+        cursor.execute(q)
+    q = 'update info set colval="1.5.1" where colkey="open-cravat"'
+    cursor.execute(q)
+    db.commit()
+    # gene
+    q = 'select * from gene limit 1'
+    cursor.execute(q)
+    cols = [v[0] for v in cursor.description]
+    cols_to_retrieve = ['base__hugo']
+    if 'base__note' in cols:
+        cols_to_retrieve.append('base__note')
+        note_to_add = False
+    else:
+        note_to_add = True
+    cursor.execute('alter table gene rename to gene_old')
+    cursor.execute('create table gene as select {} from gene_old'.format(','.join(cols_to_retrieve)))
+    if note_to_add:
+        cursor.execute('alter table gene add column base__note text')
+    cursor.execute('drop table gene_old')
+    cursor.execute('create index gene_idx_0 on gene (base__hugo)')
+    # variant_header, gene_header, mapping_header, sample_header
+    for table in ['variant', 'gene', 'mapping', 'sample']:
+        q = 'select * from {}_header'.format(table)
+        cursor.execute(q)
+        rs = cursor.fetchall()
+        cols = [v[0] for v in cursor.description]
+        if len(cols) == 2 and 'col_name' in cols and 'col_def' in cols:
+            pass
+        else:
+            q = 'alter table {}_header rename to {}_header_old'.format(table, table)
+            cursor.execute(q)
+            q = 'create table {}_header (col_name text, col_def text)'.format(table)
+            cursor.execute(q)
+            old_colkeys = ['col_name', 'col_title', 'col_type', 'col_cats', 'col_width', 'col_desc', 'col_hidden', 'col_ctg', 'col_filterable', 'col_link_format']
+            old_to_new_colkey = {'col_name': 'name', 'col_title': 'title', 'col_type': 'type', 'col_cats': 'categories', 'col_width': 'width', 'col_desc': 'desc', 'col_hidden': 'hidden', 'col_ctg': 'category', 'col_filterable': 'filterable', 'col_link_format': 'link_format'}
+            colnos = {}
+            for c in old_colkeys:
+                try:
+                    colnos[c] = cols.index(c)
+                except:
+                    colnos[c] = None
+            colidx = {}
+            for r in rs:
+                col_name = r[colnos['col_name']]
+                if table == 'gene' and col_name not in ['base__hugo', 'base__note']:
+                    continue
+                module = col_name.split('__')[0]
+                if module not in colidx:
+                    colidx[module] = 0
+                else:
+                    colidx[module] += 1
+                col_def = {}
+                for c in old_colkeys:
+                    if colnos[c] is not None:
+                        value = r[colnos[c]]
+                    else:
+                        value = None
+                    col_def[old_to_new_colkey[c]] = value
+                col_def['index'] = colidx[module]
+                col_def['genesummary'] = False
+                if col_def['categories'] is None:
+                    col_def['categories'] = []
+                else:
+                    col_def['categories'] = json.loads(col_def['categories'])
+                if col_def['width'] is None:
+                    col_def['width'] = 100
+                if col_def['hidden'] is None:
+                    col_def['hidden'] = False
+                if col_def['filterable'] is None:
+                    col_def['filterable'] = True
+                if col_def['desc'] is None:
+                    col_def['desc'] = ''
+                q = 'insert into {}_header values (\'{}\', \'{}\')'.format(table, col_name, json.dumps(col_def))
+                cursor.execute(q)
+            if table == 'gene' and note_to_add:
+                q = 'insert into gene_header (\'base__note\', \'{"name": "base__note", "index": 1, "title": "Note", "type": "string", "categories": [], "width": 50, "desc": null, "hidden": false, "category": null, "filterable": true, "link_format": null, "genesummary": false}\')'
+                cursor.execute(q)
+        q = 'drop table {}_header_old'.format(table)
+        cursor.execute(q)
+        db.commit()
+    # mapping
+    # base__fileno cannot be determined. set to 0.
+    q = 'select * from mapping limit 1'
+    cursor.execute(q)
+    cols = [v[0] for v in cursor.description]
+    if 'base__fileno' not in cols:
+        q = 'alter table mapping add column base__fileno integer'
+        cursor.execute(q)
+        q = 'update mapping set base__fileno=0'
+        cursor.execute(q)
+    db.commit()
+    # smartfilters
+    try:
+        cursor.execute('select * from smartfilters')
+    except:
+        q = 'create table smartfilters (name text, definition text)'
+        cursor.execute(q)
+        db.commit()
 
 def main ():
     args = get_args()
