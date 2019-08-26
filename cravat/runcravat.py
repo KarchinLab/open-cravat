38b9a66e01a77080a654940249a84b50770dd8a8
kmoad <kmoad@mail.insilico.us.com>
2019-08-26 12:02:35 -0600

    system ready resolution for cravat command

--- cravat/runcravat.py
+++ cravat/runcravat.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from cravat import Cravat, cravat_cmd_parser
+from cravat.admin_util import ready_resolution_console
 import asyncio
 
 def main ():
+    ready_resolution_console()
     cmd_args = cravat_cmd_parser.parse_args()
     module = Cravat(**vars(cmd_args))
     loop = asyncio.get_event_loop()
