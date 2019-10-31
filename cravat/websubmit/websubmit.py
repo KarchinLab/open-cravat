d4652dd378c93dbee00864eec3b9d878917da8d5
Rick Kim <rkim@insilico.us.com>
2019-10-30 22:12:50 -0400

    report generation adds -l hg38

--- cravat/websubmit/websubmit.py
+++ cravat/websubmit/websubmit.py
@@ -565,6 +565,7 @@ async def generate_report(request):
     run_args.extend(['--startat', 'reporter'])
     run_args.extend(['--repeat', 'reporter'])
     run_args.extend(['-t', report_type])
+    run_args.extend(['-l', 'hg38'])
     p = subprocess.Popen(run_args)
     p.wait()
     return web.json_response('done')
