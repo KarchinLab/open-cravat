9f27288ab74f54f2c4ed24610f9022f3a3ded24a
Rick Kim <rkim@insilico.us.com>
2019-11-05 10:52:27 -0500

    uses Filter by Type phrase

--- cravat/websubmit/nocache/websubmit.js
+++ cravat/websubmit/nocache/websubmit.js
@@ -883,7 +883,7 @@ function buildAnnotatorGroupSelector () {
     var div = getEl('div');
     div.className = 'div-header';
     var span = getEl('span');
-    span.textContent = 'Filter by Module\xa0\xa0';
+    span.textContent = 'Filter by Type\xa0\xa0';
     addEl(div, span);
     addEl(annotCheckDiv, div);
     var div = getEl('div');
