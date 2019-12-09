f951a1d924aef89dab37871ec318b9e56c3dd599
Kyle Moad <kmoad@insilico.us.com>
2019-12-09 17:51:28 -0500

    oc merge account commands into store

--- cravat/oc.py
+++ cravat/oc.py
@@ -9,6 +9,7 @@ version_p = root_sp.add_parser('version', parents=[cravat_admin.parser_show_vers
 
 run_p = root_sp.add_parser('run', parents=[cravat_cmd_parser], add_help=False)
 
+# module
 module_p = root_sp.add_parser('module')
 module_sp = module_p.add_subparsers()
 module_ls_p = module_sp.add_parser('ls', parents=[cravat_admin.parser_ls], add_help=False)
@@ -17,17 +18,25 @@ module_uninstall_p = module_sp.add_parser('uninstall', parents=[cravat_admin.par
 module_update_p = module_sp.add_parser('update', parents=[cravat_admin.parser_update], add_help=False)
 module_info_base_p = module_sp.add_parser('info', parents=[cravat_admin.parser_info], add_help=False)
 
+# store
 store_p = root_sp.add_parser('store')
 store_sp = store_p.add_subparsers()
 store_publish_p = store_sp.add_parser('publish', parents=[cravat_admin.parser_publish], add_help=False)
+account_create_p = store_sp.add_parser('new-account', parents=[cravat_admin.parser_create_account], add_help=False)
+account_change_pw_p = store_sp.add_parser('change-pw', parents=[cravat_admin.parser_change_password], add_help=False)
+account_reset_pw_p = store_sp.add_parser('reset-pw', parents=[cravat_admin.parser_reset_pw], add_help=False)
+account_verify_email_p = store_sp.add_parser('verify-email', parents=[cravat_admin.parser_verify_email], add_help=False)
+account_check_login_p = store_sp.add_parser('check-login', parents=[cravat_admin.parser_check_login], add_help=False)
 
-account_p = root_sp.add_parser('account')
-account_sp = account_p.add_subparsers()
-account_create_p = account_sp.add_parser('create', parents=[cravat_admin.parser_create_account], add_help=False)
-account_change_pw_p = account_sp.add_parser('change-pw', parents=[cravat_admin.parser_change_password], add_help=False)
-account_reset_pw_p = account_sp.add_parser('reset-pw', parents=[cravat_admin.parser_reset_pw], add_help=False)
-account_verify_email_p = account_sp.add_parser('verify-email', parents=[cravat_admin.parser_verify_email], add_help=False)
-account_check_login_p = account_sp.add_parser('check-login', parents=[cravat_admin.parser_check_login], add_help=False)
+#new
+new_p = root_sp.add_parser('new')
+new_sp = new_p.add_subparsers()
+new_annotator_p = new_sp.add_parser('annotator', parents=[cravat_admin.parser_new_annotator], add_help=False)
+
+# util
+util_p = root_sp.add_parser('util')
+util_sp = util_p.add_subparsers()
+util_example_input_p = util_sp.add_parser('example-input', parents=[cravat_admin.parser_make_example_input], add_help=False)
 
 
 def main():
