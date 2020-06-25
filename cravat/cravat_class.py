eab7d3a268b893c2e3df787180ecfb7a2dc1a0bf
kmoad <kyle.moad@gmail.com>
2020-06-25 13:35:57 -0400

    correct constants main_conf_path

--- cravat/cravat_class.py
+++ cravat/cravat_class.py
@@ -650,7 +650,7 @@ class Cravat (object):
             else:
                 msg = 'Genome assembly should be given (as one of {}) with -l option or a default genome assembly should be defined in {} as default_assembly.'.format(
                     ', '.join(constants.assembly_choices), 
-                    constants.cravat_conf_path,
+                    constants.main_conf_path,
                 )
                 print(msg)
                 exit()
