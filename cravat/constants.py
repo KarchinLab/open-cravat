29d7b2cd341eef28c06a2819d4dc86804e03cee3
kmoad <kyle.moad@gmail.com>
2020-11-17 16:43:06 -0500

    send only cannonical_chroms to annots by defaults
    annots can change chrom list by setting self.supported_chroms

--- cravat/constants.py
+++ cravat/constants.py
@@ -277,3 +277,5 @@ assembly_choices = ['hg38', 'hg19', 'hg18']
 publish_time_fmt = '%Y-%m-%dT%H:%M:%S.%f%z'
 
 install_tempdir_name = 'temp'
+
+cannonical_chroms = ['chr'+str(n) for n in range(1,23)]+['chrX','chrY']
