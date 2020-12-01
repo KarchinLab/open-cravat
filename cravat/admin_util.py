6240191db57d439fd38d68bf77885003c7505b71
Rick Kim <rkim@insilico.us.com>
2020-12-01 11:12:14 -0500

    md file read with utf-8

--- cravat/admin_util.py
+++ cravat/admin_util.py
@@ -87,7 +87,7 @@ class LocalModuleInfo (object):
         self.readme_path = os.path.join(self.directory, self.name+'.md')
         self.readme_exists = os.path.exists(self.readme_path)
         if self.readme_exists:
-            with open(self.readme_path) as f:
+            with open(self.readme_path, encoding='utf-8') as f:
                 self.readme = f.read()
         else:
             self.readme = ''
