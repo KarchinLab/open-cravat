0d08c0131d0bff4fa82cc3a79f45e97184f11684
Kyle Anderson <kanderson@potomacitgroup.com>
2023-02-09 16:06:16 -0500

    Fix assert expected/actual placement

--- tests/test_admin_util.py
+++ tests/test_admin_util.py
@@ -23,7 +23,7 @@ class TestAdminUtil(unittest.TestCase):
         mock_get_remote_module_info.return_value = remote_module_info_one_version
         
         deps = get_install_deps(module_name="A", version="1.0")
-        self.assertEqual(deps, {})
+        self.assertEqual({}, deps)
 
     
     @patch('cravat.admin_util.get_remote_module_info', name="direct_rmi")
