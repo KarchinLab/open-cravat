6df057a87de30aeecc02149881f4d038a811e2f3
Kyle Anderson <kanderson@potomacitgroup.com>
2023-07-26 17:35:46 -0400

    Move changePage to core

--- cravat/websubmit/nocache/websubmit.js
+++ cravat/websubmit/nocache/websubmit.js
@@ -1717,27 +1717,6 @@ function transitionToSettings () {
     settingsdiv.style.display = 'block';
 }
 
-function changePage (selectedPageId) {
-    var pageselect = document.getElementById('pageselect');
-    var pageIdDivs = pageselect.children;
-    for (var i = 0; i < pageIdDivs.length; i++) {
-        var pageIdDiv = pageIdDivs[i];
-        var pageId = pageIdDiv.getAttribute('value');
-        var page = document.getElementById(pageId);
-        if (page.id == selectedPageId) {
-            page.style.display = 'block';
-            pageIdDiv.setAttribute('selval', 't');
-            if (selectedPageId == 'storediv') {
-                OC.currentTab = 'store';
-            } else if (selectedPageId == 'submitdiv') {
-                OC.currentTab = 'submit';
-            }
-        } else {
-            page.style.display = 'none';
-            pageIdDiv.setAttribute('selval', 'f');
-        }
-    }
-}
 
 function openSubmitDiv () {
     var div = document.getElementById('submitcontentdiv');
