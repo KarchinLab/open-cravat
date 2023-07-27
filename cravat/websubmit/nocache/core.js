c5de0b60f36db5e40355d7b90475711f65888a98
Kyle Anderson <kanderson@potomacitgroup.com>
2023-07-27 13:41:16 -0400

    Add PubSub mediator class for cross-module communication

--- cravat/websubmit/nocache/core.js
+++ cravat/websubmit/nocache/core.js
@@ -38,3 +38,44 @@ function changePage (selectedPageId) {
         }
     }
 }
+
+/************** Mediator / PubSub ****************/
+/***
+ * PubSub is a simple publish / subscribe mediator pattern module to facilitate cross-module communication within the app.
+ * A global mediator should be stored to the OC namespace, modules should subscribe to events they are interested in, then
+ * other modules can publish to that event. The PubSub class has one event channel by default named 'error' and a default
+ * handler for the 'error' event that will log the error arguments to the console.
+ *
+ * Example usage:
+ * ```js
+ * const mediator = new PubSub();
+ * mediator.subscribe('error', showErrorDialog); // showErrorDialog would be an example hanlder function
+ * mediator.publish('error', 'Test error', 'Error Details', 42); // publish an event on the 'error' channel; any number of args
+ * mediator.subscribe('otherModuleChanged', updateHandler);
+ * ```
+ */
+class PubSub {
+    constructor() {
+        this.channels = {};
+        this.channels.error = [];
+        this.channels.error.push(function(...args) {
+            console.log(`Error: ${args}`);
+        });
+    }
+
+    subscribe(channel, callback) {
+        if (!this.channels.hasOwnProperty(channel)) {
+            this.channels[channel] = [];
+        }
+        this.channels[channel].push(callback);
+    }
+
+    publish(channel, ...args) {
+        if (!this.channels.hasOwnProperty(channel)) {
+            this.channels[channel] = [];
+        }
+				for (let callback of this.channels[channel]) {
+            callback(...args);
+        }
+    }
+}
