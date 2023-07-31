4a22c75b5a1dd4f1cb43fd6487360256b15490df
Kyle Anderson <kanderson@potomacitgroup.com>
2023-07-31 13:47:11 -0400

    Add strict mode to all webstore/submit js files

--- cravat/websubmit/nocache/core.js
+++ cravat/websubmit/nocache/core.js
@@ -1,11 +1,11 @@
+'use strict';
+
 function getEl(tag) {
-    var new_node = document.createElement(tag);
-    return new_node;
+    return document.createElement(tag);
 }
 
 function getTn(text) {
-    var new_text_node = document.createTextNode(text);
-    return new_text_node;
+    return document.createTextNode(text);
 }
 
 function addEl(pelem, child) {
@@ -18,18 +18,18 @@ function addEl(pelem, child) {
  * @param selectedPageId - the name of the page to change to. valid options are 'storediv', 'submitdiv'
  */
 function changePage (selectedPageId) {
-    var pageselect = document.getElementById('pageselect');
-    var pageIdDivs = pageselect.children;
-    for (var i = 0; i < pageIdDivs.length; i++) {
-        var pageIdDiv = pageIdDivs[i];
-        var pageId = pageIdDiv.getAttribute('value');
-        var page = document.getElementById(pageId);
-        if (page.id == selectedPageId) {
+    const pageSelect = document.getElementById('pageselect');
+    const pageIdDivs = pageSelect.children;
+    for (let i = 0; i < pageIdDivs.length; i++) {
+        const pageIdDiv = pageIdDivs[i];
+        const pageId = pageIdDiv.getAttribute('value');
+        const page = document.getElementById(pageId);
+        if (page.id === selectedPageId) {
             page.style.display = 'block';
             pageIdDiv.setAttribute('selval', 't');
-            if (selectedPageId == 'storediv') {
+            if (selectedPageId === 'storediv') {
                 OC.currentTab = 'store';
-            } else if (selectedPageId == 'submitdiv') {
+            } else if (selectedPageId === 'submitdiv') {
                 OC.currentTab = 'submit';
             }
         } else {
