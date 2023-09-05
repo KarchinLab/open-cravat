2690f0e834ccf4f8c0ff9e30868ccee8da4f7cd8
jeltje <yinnah@yahoo.com>
2023-09-05 11:48:30 -0700

    added translocations. Next: refactor to pull overlaps out of mateObject, or add mate info to the variant class

--- sv/vcfOverlap.py
+++ sv/vcfOverlap.py
@@ -4,7 +4,6 @@ import sys, os, re, argparse, textwrap
 import vcf
 from bedparse import chromIntervalTreesFromBed
 from intervaltree import IntervalTree
-#import pickle
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -15,23 +14,19 @@ Keep in mind that bed is 0 based and VCF is 1 based
 
         '''))
 group = parser.add_argument_group('required arguments')
-group.add_argument('vcffile', type=str, help='VCF file')
 group.add_argument('bedfile', type=str, help='Bed file')
+group.add_argument('vcffile', type=str, help='VCF file')
 
 # TODO: deal with chr vs nonchr
 
 is_paired = []
 def vcfOverlap(bedfile, vcffile):
     bedExonTree, bedTxTree = chromIntervalTreesFromBed(bedfile)
-#    with open('bedfile.pkl', 'wb') as pfile:
-#        pickle.dump([bedExonTree, bedTxTree], pfile)
-#    with open('bedfile.pkl', 'rb') as pfile:
-#        [bedExonTree, bedTxTree] = pickle.load(pfile)
     chroms = bedTxTree.keys()
     # attempt to speed up lookups (VCF is generally in chr order)
     curChrom, exonTree, txTree = None, None, None
     # header
-    print('SVTYPE\tID\tMATEID\tCHROM\tPOS1\tPOS2\tSIZE\tDEL_GENES\tBROKEN_GENES')
+    print('SVTYPE\tID\tMATEID\tCHROM\tMATECHROM\tPOS1\tPOS2\tSIZE\tDEL_GENES\tBROKEN_GENES')
     with open(vcffile, 'r') as f:
         reader = vcf.Reader(f)
         for variant in reader:
@@ -41,19 +36,14 @@ def vcfOverlap(bedfile, vcffile):
              if variant.ID in is_paired:
                  # we already have info for this variant (this may not remain true, deal with later)
                  continue
-             print(variant.ID)
              if not variant.CHROM in chroms:
-                 print('WARNING, chromosome not found in bed file:', variant.CHROM, file=sys.stderr)
+                 sys.stderr.write('WARNING, chromosome not found in bed file:', variant.CHROM)
                  continue
              if variant.CHROM != curChrom:
                  exonTree = bedExonTree[variant.CHROM]
                  txTree = bedTxTree[variant.CHROM]
                  curChrom = variant.CHROM
-                 print(curChrom)
-             # the below first prints all genes that are overlapped 
-             # and then all the exons (with their transcript IDs). 
-             # We might want to return a dict of exontrees from bedparse with gene ID as keys instead.
-             mate_id = print_info(variant, txTree, exonTree)
+             mate_id = print_info(variant, txTree, exonTree, bedTxTree)
              if mate_id:
                  is_paired.append(mate_id)
 
@@ -61,7 +51,7 @@ def vcfOverlap(bedfile, vcffile):
 # ALT can have multiple entries in complex rearrangements, see p24 in
 # https://samtools.github.io/hts-specs/VCFv4.4.pdf
 # we might want to skip those
-def print_info(variant, txTree, exonTree):
+def print_info(variant, txTree, exonTree, bedTxTree):
     '''Extract information from variant and overlapping genes for printing. Returns mate.ID if one is found'''
     # if this variant has a mate we must add its info
     # currently we only use info we extract from the variant, and mark the mate ID
@@ -69,10 +59,14 @@ def print_info(variant, txTree, exonTree):
     variant.interrupted_genes = IntervalTree()
     variant.interrupted_genes.update(txTree[variant.POS])
     if 'MATEID' in variant.INFO:
-        mate = mateinfo(variant, txTree)
-        print(f"{mate.SVTYPE}\t{variant.ID}\t{mate.ID}\t{variant.CHROM}\t{variant.POS}\t{mate.POS}", end='')
+        mate = mateinfo(variant, txTree, bedTxTree)
+        broken_genes = ';'.join([x.data for x in mate.interrupted_genes]) if mate.interrupted_genes else 'None'
+        del_genes = ';'.join([x.data for x in mate.deleted_genes]) if mate.deleted_genes else 'None'
+        print(f"{mate.SVTYPE}\t{variant.ID}\t{mate.ID}\t{variant.CHROM}\t{mate.CHROM}\t{variant.POS}\t{mate.POS}", end='')
         if mate.SVTYPE == 'SVDEL':
-            print(f"\t{mate.POS-variant.POS}\t{mate.del_genes}\t{mate.broken_genes}")
+            print(f"\t{mate.POS-variant.POS}\t{del_genes}\t{broken_genes}")
+        elif mate.SVTYPE == 'BND':
+            print(f"\tNA\tNA\t{broken_genes}")
         else:
             print('')
         return mate.ID
@@ -93,13 +87,17 @@ def print_info(variant, txTree, exonTree):
 
 class mateObject():
     '''mate information extraced from variant'''
-    def __init__(self, name, chrom, coord, txTree, allele):
+    def __init__(self, name, chrom, coord, bedTxTree, allele):
         self.ID = name[0]
         self.CHROM = chrom
         self.POS = coord
-        self.SVTYPE = allele
-        self.interrupted_genes = txTree[self.POS]
-    def add_deltype(self, variant, txTree):
+        self.SVTYPE = 'UNKNOWN_'+allele
+        self.interrupted_genes = IntervalTree()
+        self.deleted_genes = IntervalTree()
+        if self.CHROM in bedTxTree:
+            txTree = bedTxTree[self.CHROM]
+            self.interrupted_genes = txTree[self.POS]
+    def is_deletion(self, variant, txTree):
         self.SVTYPE = 'SVDEL'
         # check for region overlaps
         start = min(variant.POS, self.POS)
@@ -111,16 +109,15 @@ class mateObject():
             pass
         else:
             self.interrupted_genes.update(variant.interrupted_genes)
-            self.del_genes = ';'.join([x.data for x in self.deleted_genes])
-        if not self.del_genes:
-            self.del_genes = 'None'
-        self.broken_genes = ';'.join([x.data for x in self.interrupted_genes])
-        if not self.broken_genes:
-            self.broken_genes = 'None'    
+    def is_translocation(self):
+        self.SVTYPE = 'BND'
 
 
-def mateinfo(variant, txTree):
-    '''get type of SV from ALT field info if the variant has a mate'''
+# TODO: decide to keep or ditch having txTree separately
+# if it's a DEL it's on the same chr, so same bedTxTree[chr] so we wouldn't have to try it
+# but if it's a BND we need another bedTxTree[chr] (and test if it exists)
+def mateinfo(variant, txTree, bedTxTree):
+    '''parse ALT field if the variant has a mate'''
     # Find chr and coordinate in ALT field WARNING we must have chr in the chrom ID
     pattern = r'(chr\w+):(\d+)'
     match = re.search(pattern, str(variant.ALT[0]))
@@ -128,27 +125,26 @@ def mateinfo(variant, txTree):
         chrom = match.group(1)
         coord = int(match.group(2))
     else:
-        print("No match found, cannot make MATE.", variant.ALT)
-        sys.exit()
+        sys.stderr.write("WARNING, No match found, cannot make MATE.", variant.ALT)
         return False
-    mate = mateObject(variant.INFO['MATEID'], chrom, coord, txTree, variant.ALT[0])
-    # NOTE must add other types
-    add_info_if_del(variant, mate, txTree)
-    return mate
+    mate = mateObject(variant.INFO['MATEID'], chrom, coord, bedTxTree, str(variant.ALT[0]))
 
-# could add this as a mate.function()
-# simplified this from an earlier version: consider this a DEL if
-# there is [chr:pos[ or ]chr:pos]. TODO: figure out what every combination is
-def add_info_if_del(variant, mate, txTree):
-    '''See if the variant and mate are a deletion on a single chromosome'''
-    # we're currently ignoring any inserts, which are strings before [ or after ]
-#    simple_del = f"{variant.REF}\\[{variant.CHROM}:{mate.POS}\\["
+    ## DELETION ##
+    # if mate and variant are on the same chromosome, this is a deletion
+    # simplified this from an earlier version: consider this a DEL if
+    # there is [chr:pos[ or ]chr:pos]. TODO: figure out what every combination is
     pattern = f"[\\[\\]]{variant.CHROM}:{mate.POS}[\\[\\]]"
     match = re.search(pattern, str(variant.ALT[0]))
     if match:
-        mate.add_deltype(variant, txTree)
-        return True
-    return False
+        mate.is_deletion(variant, txTree)
+        return mate
+    # sanity check
+    if mate.CHROM == variant.CHROM:
+        sys.stderr.write("Missed something, is this a deletion?", variant.ALT, variant.CHROM, variant.POS)
+    ## TRANSLOCATION ##
+    # if the chromosome is different this is a translocation
+    mate.is_translocation()
+    return mate
 
 # note: https://gatk.broadinstitute.org/hc/en-us/articles/5334587352219-How-to-interpret-SV-VCFs
 # https://samtools.github.io/hts-specs/VCFv4.4.pdf p22
