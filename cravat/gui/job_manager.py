170bba9284150ec39eca70b027c461aa3703b259
jhiggins@potomacitgroup.com <jhiggins@potomacitgroup.com>
2024-04-12 13:52:53 -0400

    Initial job integration

---
+++ cravat/gui/job_manager.py
@@ -0,0 +1,65 @@
+import celery
+from celery import current_app, states
+
+
+def celery_init_app(flask_app):
+    class FlaskTask(celery.Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with flask_app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = celery.Celery(flask_app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(flask_app.config["CELERY"])
+    celery_app.set_default()
+    flask_app.extensions["celery"] = celery_app
+    return celery_app
+
+
+class Task(object):
+    def __init__(self, task_id):
+        self.id = task_id
+
+    @property
+    def result(self):
+        return current_app.AsyncResult(self.id)
+
+    @property
+    def aborted(self):
+        return self.result.state in [
+            states.REVOKED,
+            states.REJECTED,
+            'ABORTED',
+            states.IGNORED
+        ]
+
+    @property 
+    def cravat_status(self):
+        {
+            # PENDING is complicated.
+            #
+            # If we send a task to celery, and it
+            # hasn't been received by a worker yet, it's pending.
+            #
+            # If we ask for a status of an unknown task id, celery
+            # assumes you know what you are asking for and replies
+            # `pending`, so pending may mean celery has no idea what
+            # you are talking about.
+            #
+            # I think the assumption is safe, and we can say submitted,
+            # however, if we run into a case of lost job, we may need
+            # to reach into the queue and go looking on "PENDING"
+            states.PENDING: 'Submitted',
+            states.RECEIVED: 'Starting',
+            states.STARTED: 'Running',
+            states.SUCCESS: 'Finished',
+            states.FAILURE: 'Error',
+            states.REVOKED: 'Aborted',
+            states.REJECTED: 'Aborted',
+            states.RETRY: 'Submitted',
+            states.IGNORED: 'Aborted',
+            'ABORTED': 'Aborted',
+        }.get(self.result.state, 'Aborted')
+        
+    def cancel(self):
+        if not self.result.ready():
+            self.result.revoke()
