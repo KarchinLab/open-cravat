b56d5ed675a066bdafd488f0e845abf006bc7a71
jhiggins@potomacitgroup.com <jhiggins@potomacitgroup.com>
2024-04-29 17:30:08 -0400

    Add logout

--- cravat/gui/multiuser/routes.py
+++ cravat/gui/multiuser/routes.py
@@ -5,6 +5,7 @@ from cravat.gui.routing import relative_router
 def load(application):
     server_router = relative_router("/server", application)
     server_router(r'/login', None, login)
+    server_router(r'/logout', None, logout)
     server_router(r'/usersettings', None, get_user_settings)
     server_router(r'/signup', None, signup)
     server_router(r'/checklogged', None, check_logged)
