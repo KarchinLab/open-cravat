224f6dc1ebe1b731436f703739b9391d7945db9e
jhiggins@potomacitgroup.com <jhiggins@potomacitgroup.com>
2024-04-30 16:32:24 -0400

    Updated some documentation on how to get things up and running for fresh developers.

--- cravat/gui/README.md
+++ cravat/gui/README.md
@@ -1,4 +1,24 @@
 # Commands
+## Running the GUI
+To start the GUI in Flask, run:
+
+```
+python -m pip install -e .
+oc gui
+```
+
+If for some reason you want to run the GUI in aiohttp:
+
+```
+oc gui --aiohttp
+```
+
+at some point in the future aiohttp support will break and not come back.
+
+## Job Workers
+
+If the GUI is running outside multi-user mode then it will spawn celery 
+workers automatically (see `cravat_web.py`).
 
 To start Celery, while in multiuser, in a separate process:
 
@@ -7,4 +27,5 @@ celery -A cravat.gui.celery worker
 ```
 
 You can run `celery --help` for other options like restricting
-queues
+queues.  Here `cravat.gui.celery` is the dotted Python path to our configured 
+celery `App` instance.
--- cravat/gui/config.py
+++ cravat/gui/config.py
@@ -7,6 +7,30 @@ CRAVAT_SYSCONF = admin_util.get_system_conf()
 default_exchange = Exchange('default', type='direct')
 management_exchange = Exchange('management', type='direct')
 
+# COOKIE CONFIGURATION (https://flask.palletsprojects.com/en/2.3.x/config/)
+
+# This is the string used for signing the session cookies, it should not be stored in the codebase, in a
+# multiuser server scenario this was loaded from the admin database.  A cleaner solution may be to
+# load it from an environment variable.
+
+# You may generate a value by using:
+# $ python -c 'import secrets; print(secrets.token_hex())'
+# '192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
+# SECRET_KEY = 'a random string'
+
+SESSION_COOKIE_NAME = 'session'
+
+# None is the default and forces the browser to only send cookies to the exact domain name from which it was set
+SESSION_COOKIE_DOMAIN = None
+
+# flag to make the cookie is inaccessible from Javascript
+SESSION_COOKIE_HTTPONLY = True
+
+# Set the secure flag on the cookie. False is the default value, and needed for the
+# laptop use case but for a server deployment behind SSL this should be True.
+# again, an excellent candidate for configuration via environment variable.
+SESSION_COOKIE_SECURE = False
+
 CELERY = dict(
     broker_url='filesystem://',
     broker_transport_options={
