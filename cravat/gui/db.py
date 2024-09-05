727c9800d24632a114b0a79bf991d4df41f58f2b
jhiggins@potomacitgroup.com <jhiggins@potomacitgroup.com>
2024-09-05 16:12:10 -0400

    Smartfilters endpoint

--- cravat/gui/db.py
+++ cravat/gui/db.py
@@ -1,4 +1,9 @@
 from sqlite3 import connect
 
+def table_exists (cursor, table):
+    q = 'select name from sqlite_master where type="table" and ' +\
+        'name="' + table + '"'
+    cursor.execute(q)
+    r = cursor.fetchone()
 
-
+    return r is not None
--- cravat/gui/result/routes.py
+++ cravat/gui/result/routes.py
@@ -7,4 +7,5 @@ def load(application):
     router('/service/getresulttablelevels', None, get_result_levels)
     router('/service/variantcols', None, get_variant_cols)
     router('/service/widgetlist', None, get_widgets)
+    router('/service/smartfilters', None, get_smartfilters)
     router('/widgetfile/<module>/<filename>', None, serve_widgetfile)
