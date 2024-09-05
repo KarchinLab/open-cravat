a3e8335c470018e9a995bfd3ff4f0b5d2ada18e8
jhiggins@potomacitgroup.com <jhiggins@potomacitgroup.com>
2024-09-05 16:25:35 -0400

    loadfiltersettings endpoint

--- cravat/gui/result/routes.py
+++ cravat/gui/result/routes.py
@@ -1,7 +1,6 @@
 from .handlers import *
 from cravat.gui.routing import relative_router
 
-
 def load(application):
     router = relative_router("/result", application)
     service_router = relative_router("/result/service", application)
@@ -11,4 +10,6 @@ def load(application):
     service_router('/widgetlist', None, get_widgets)
     service_router('/smartfilters', None, get_smartfilters)
     service_router('/samples', None, get_samples)
+    service_router('/loadfiltersetting', None, load_filter_setting)
+
     router('/widgetfile/<module>/<filename>', None, serve_widgetfile)
