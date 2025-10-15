2e6731567184ccb78f25f7f944738d80401d6dc9
Kyle Moad <kyle.moad@gmail.com>
2025-10-15 18:39:06 -0400

    bump version to 2.17.0

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ for root, dirs, files in os.walk(os.path.join('cravat', 'websubmit')):
 setup(
     name='open-cravat',
     packages=['cravat'],
-    version='2.16.0',
+    version='2.17.0',
     description='OpenCRAVAT - variant analysis toolkit',
     long_description=readme(),
     author='Kyle Moad, Kyle Anderson, Charlie Vaske, Jasmine Baker, and Rachel Karchin',
